7a4f52970fbf41badd153e1b2876b266dc1e7883
Jing Cui <jingcui99@gmail.com>
2024-05-29 14:32:48 -0400

    fix warnings

--- src/main/java/com/example/movies/MovieController.java
+++ src/main/java/com/example/movies/MovieController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.bson.types.ObjectId;
 import java.util.Optional;
 import java.util.List;
 
--- src/main/java/com/example/movies/MovieRepository.java
+++ src/main/java/com/example/movies/MovieRepository.java
@@ -2,8 +2,6 @@ package com.example.movies;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
 import java.util.Optional;
 
 public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
